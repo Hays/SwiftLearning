d4e053d4efb7ad615028aa2b7fa25a8ba0fed2b4
Hays <huangwenxi@cvte.com>
2018-04-11 17:00:45 +0800

    [Modify]init project

---
+++ SwiftLearning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftLearning
+//
+//  Created by Hays on 2018/4/11.
+//  Copyright © 2018 MAXHUB. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftLearning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by Hays on 2018/4/11.
+//  Copyright © 2018 MAXHUB. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
